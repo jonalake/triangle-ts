bd22a15bc158d369cafde4696a9c2af1a543635e
Jon Lake <jlake21@ford.com>
2022-05-06 12:32:32 -0600

    Added Solution

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,41 @@
-export class Triangle {}
+export class Triangle {
+  side1: number;
+  side2: number;
+  side3: number;
+  constructor(sideOne: number, sideTwo: number, sideThree: number) {
+    this.side1 = sideOne;
+    this.side2 = sideTwo;
+    this.side3 = sideThree;
+  }
+  isEquilateral() {
+    return (
+      this.side1 === this.side2 && this.side1 === this.side3 && this.side1 !== 0
+    );
+  }
+  isIsosceles() {
+    return (
+      (this.side1 === this.side2 ||
+        this.side1 === this.side3 ||
+        this.side2 === this.side3) &&
+      this.side1 !== 0 &&
+      this.side2 !== 0 &&
+      this.side3 !== 0 &&
+      this.side1 + this.side2 > this.side3 &&
+      this.side1 + this.side3 > this.side2 &&
+      this.side2 + this.side3 > this.side1
+    );
+  }
+  isScalene() {
+    return (
+      this.side1 !== this.side2 &&
+      this.side1 !== this.side2 &&
+      this.side2 !== this.side3 &&
+      this.side1 !== 0 &&
+      this.side2 !== 0 &&
+      this.side3 !== 0 &&
+      this.side1 + this.side2 > this.side3 &&
+      this.side1 + this.side3 > this.side2 &&
+      this.side2 + this.side3 > this.side1
+    );
+  }
+}
